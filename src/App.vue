b5506d4f933609a51978bdadd0e00d2b734a1f22
hhm-lxb <lxbmisszsj@gmail>
2021-09-29 13:12:14 +0800

    j

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  methods: {
+    get () {
+      axios.get('http://localhost:3000/templateinfo').then(res => {
+        console.log(res)
+      })
+    }
+  },
+  created () {
+    this.get()
+  }
+}
+</script>
